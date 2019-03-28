10866ef37e8721e18b46ae2eb38793c26818c2ba
Josh Miklos <josh.miklos@gmail.com>
2019-03-28 00:06:50 -0700

    I feel like this autoencoder is now alive. It's scary...

--- autoocr/encoder/encoder.py
+++ autoocr/encoder/encoder.py
@@ -1,63 +1,146 @@
 from torch import nn
 import torch
 import numpy as np
+import math
+from torch import autograd
+from threading import Thread
+import torch.optim as optim
+import os
+from autoocr.otherlib.kwinners import KWinnersTakeAll, KWinnersBoost
+
+if False:
+    from typing import Any
+
+
+def dense_to_sparse(x):
+    """ converts dense tensor x to sparse format """
+    # from: https://discuss.pytorch.org/t/how-to-convert-a-dense-matrix-to-a-sparse-one/7809/3
+    x_typename = torch.typename(x).split('.')[-1]
+    sparse_tensortype = getattr(torch.sparse, x_typename)
+
+    indices = torch.nonzero(x)
+    if len(indices.shape) == 0:  # if all elements are zeros
+        return sparse_tensortype(*x.shape)
+    indices = indices.t()
+    values = x[tuple(indices[i] for i in range(indices.shape[0]))]
+    return sparse_tensortype(indices, values, x.size())
+
+
+stopper = False
+
 
 class ConvolutionalEncoder(nn.Module):  # NOSONAR
-    def __init__(self):
+    def __init__(self, displayable=False):
         super(ConvolutionalEncoder, self).__init__()
-        self.fc = nn.Conv2d(1, 1000, kernel_size=(16, 16))
-        self.fc_act = nn.LeakyReLU()
-        self.conv = nn.ConvTranspose2d(1000, 1, kernel_size=(16, 16))
-        self.conv_norm = nn.BatchNorm2d(1)
-        self.conv_act = nn.ReLU(True)
+        self.fc = nn.Conv2d(1, 10000, kernel_size=(16, 16))
+        self.booster = KWinnersBoost()
+        self.conv = nn.ConvTranspose2d(10000, 1, kernel_size=(16, 16))
+        self.displayable = displayable
+        if self.displayable:
+            self.display_list = [np.empty((16, 16)),
+                                 np.empty((100, 100)),
+                                 np.empty((100, 100)),
+                                 np.empty((100, 100)),
+                                 np.empty((16, 16)), ]
+
+    def _display_video_threads(self):
+        if self.displayable:
+            threads = []
+            for d in self.display_list:
+                v = VideoHandlerThread(video_source=d, callbacks=display_callbacks)
+                threads.append(v)
+            return threads
+
+    def _display_windows(self):
+        s = SubscriberWindows(video_sources=self.display_list,
+                              window_names=[str(x) for x in range(len(self.display_list))])
+        return s
+
+    def display(self, training_function, *args, **kwargs):
+        global stopper
+        t = Thread(target=training_function, args=args, kwargs=kwargs)
+        vs = self._display_video_threads()
+        w = self._display_windows()
+        for v in vs:
+            v.start()
+        t.start()
+        w.loop()
+        for v in vs:
+            v.join(timeout=0.1)
+        stopper = True
+        t.join(timeout=0.1)
 
     def forward(self, x):
+        if self.displayable: self.display_list[0][...] = x.view(16, 16).data.cpu().numpy()
         x = self.fc(x)
-        x = self.fc_act(x)
+        x_max = torch.max(x.data)
+        x_min = torch.min(x.data)
+        x_display = ((x.data - x_min) * (1.0 / (x_max - x_min))).view(100, 100).cpu().numpy()
+
+        if self.displayable: self.display_list[1][...] = x_display*1.0
+
+        x = self.booster(x)
+        x_max = torch.max(self.booster.boost_tensor.data)
+        x_min = torch.min(self.booster.boost_tensor.data)
+        x_display = ((self.booster.boost_tensor.data - x_min)*(1.0/(x_max-x_min))).view(100, 100).cpu().numpy()
+        if self.displayable: self.display_list[2][...] = x_display*1.0
+        x_max = torch.max(x.data)
+        x_min = torch.min(x.data)
+        x_display = ((x.data - x_min) * (1.0 / (x_max - x_min))).view(100, 100).cpu().numpy()
+
+        if self.displayable: self.display_list[3][...] = x_display*1.0
+
+        # x = dense_to_sparse(x) Todo: implement convTranspose on sparse for 5-10x speedup here.
         x = self.conv(x)
-        x = self.conv_norm(x)
-        x = self.conv_act(x)
+        if self.displayable: self.display_list[4][...] = x.view(16, 16).data.cpu().numpy()
         return x
 
 
-loss_fn = nn.MSELoss(reduction='sum')
-learning_rate = 1e-4
+loss_fn = nn.MSELoss()
+learning_rate = 1.0
 
 from autoocr.datasets import unifoundry as uni
 from itertools import count
 from cvpubsubs.webcam_pub import VideoHandlerThread, display_callbacks
 from cvpubsubs.window_sub import SubscriberWindows
+
+
 def train_from_unicode():
     it = uni.shuffled_iter()
-    model = ConvolutionalEncoder()
-    device = torch.device('cpu')
 
-    x = torch.randn((16, 16), device=device)
-    y = torch.randn((16, 16), device=device)
-    arr = np.empty((2,16,16, 1))
-    def train_display(frame, id):
+    model = ConvolutionalEncoder(displayable=True)
+    if os.path.isfile('AutoOCR.torch'):
+        model_dict = torch.load('AutoOCR.torch')
+        model_dict.pop('booster.boost_tensor')
+        model.load_state_dict(model_dict)
+
+    model = model.cuda()
+    optimizer = optim.Adam(model.parameters(), amsgrad=True)
+
+    arr = np.empty((2, 16, 16, 1))
+
+    def train_display(it):
+        inp = next(it)
         for s in count(0):
-            inp = next(it)
-            arr[1, :] = inp.copy().astype(np.float)[..., np.newaxis]
-            y_pred = model(torch.Tensor(inp.astype(np.float)[np.newaxis, np.newaxis, ...]))
-            loss = loss_fn(y_pred, y)
-            #print(s, loss.item())
-            model.zero_grad()
-            loss.backward()
-            with torch.no_grad():
-                for param in model.parameters():
-                    param.data -= learning_rate * param.grad
-            arr[0,:] = y_pred.detach().permute((0,2,3,1)).cpu().numpy()[0,...]
-            return arr
+            if s % 10 == 0:
+                inp = next(it)
+            arr[0, :] = inp.copy().astype(np.float)[..., np.newaxis]
+            with torch.autograd.set_grad_enabled(True):
+                inp_tens = torch.Tensor(inp.astype(np.float)[np.newaxis, np.newaxis, ...]).cuda()
+                y_pred = model(inp_tens)
+                loss = loss_fn(y_pred, inp_tens)
+                # print(s, loss.item())
+                model.zero_grad()
+                loss.backward()
+                optimizer.step()
+            if stopper:
+                break
 
-    v = VideoHandlerThread(video_source=arr, callbacks=display_callbacks + [train_display])
-    s = SubscriberWindows(video_sources=[arr], window_names=[str(x) for x in range(2)])
-
-    v.start()
-    s.loop()
-    v.join()
-
+    model.display(train_display, it)
+    torch.save(model.state_dict(), "AutoOCR.torch")
 
 
 if __name__ == '__main__':
+
     train_from_unicode()
+
---
+++ autoocr/otherlib/kwinners.py
@@ -0,0 +1,96 @@
+import torch
+from torch import autograd
+from torch import nn
+import math
+
+
+# boost -> lateral-inhibit -> k-winners
+# boost (update boosting) <- k-winners (winners)
+# lateral-inhibit (store losers, update chokes) <- k-winners (winners)
+
+# todo: create KWinnersBoose _Function_, not module, and implement backprop in it.
+class KWinnersTakeAll(autograd.Function):
+    # adapted from: https://discuss.pytorch.org/t/k-winner-take-all-advanced-indexing/24348
+
+    @staticmethod
+    def backward(ctx, *grad_outputs):
+        result, = ctx.saved_tensors
+        backprop = grad_outputs[0] * result
+        # print(backprop)
+        return backprop, None, None
+
+    @staticmethod
+    def forward(ctx, tensor, sparsity):  # type: (Any, torch.Tensor, float) -> torch.Tensor
+        batch_size, embedding_size = tensor.shape[:2]
+        if boost is not None:
+            boosted = tensor + boost
+        else:
+            boosted = tensor
+        boosted = boosted.clone()
+        _, argsort = boosted.sort(dim=1, descending=True)
+        k_active = math.ceil(sparsity * embedding_size)
+        active_indices = argsort[:, :k_active]
+        mask_active = torch.ByteTensor(tensor.shape).zero_()
+        mask_active[torch.arange(batch_size).unsqueeze(1), active_indices] = 1
+        tensor[~mask_active] = 0
+        ctx.save_for_backward(tensor)
+        return tensor
+
+
+class _KWinnersBoostFunc(autograd.Function):
+    # adapted from: https://discuss.pytorch.org/t/k-winner-take-all-advanced-indexing/24348
+
+    @staticmethod
+    def backward(ctx, *grad_outputs):
+        result, boost_tensor, boost_percent = ctx.saved_tensors
+        backprop = grad_outputs[0] * result
+        return backprop, None, None, None, None
+
+    @staticmethod
+    def forward(ctx, tensor, sparsity, boost_tensor, boost_percent,
+                boost_method):  # type: (Any, torch.Tensor, float) -> torch.Tensor
+        batch_size, embedding_size = tensor.shape[:2]
+        boost_tensor = boost_method(tensor, boost_tensor, boost_percent)
+        tensor_above = torch.where(tensor > 0, tensor, torch.zeros_like(tensor))
+        boosted = tensor_above + boost_tensor
+        boosted = boosted.clone()
+        test, argsort = boosted.sort(dim=1, descending=True)
+        k_active = math.ceil(sparsity * embedding_size)
+        active_indices = argsort[:, :k_active]
+        mask_active = torch.ByteTensor(tensor.shape).zero_()
+        mask_active[torch.arange(batch_size).unsqueeze(1), active_indices] = 1
+        tensor[~mask_active] = 0
+        tensor[mask_active] = 1
+        ctx.save_for_backward(tensor, boost_tensor, boost_percent)
+        boost_tensor[mask_active] = 0
+
+        return tensor, boost_tensor
+
+
+class PercentOfMaxBoosting(object):
+    def __call__(self, input_tensor, boost_tensor, boost_percent):
+        # type: (torch.Tensor, torch.Tensor, float) -> (torch.Tensor, torch.Tensor)
+        max_val = torch.max(input_tensor.clone())
+        max_val = torch.max(torch.zeros_like(max_val), max_val)
+        boost_plus = max_val * boost_percent
+        out_boost = boost_tensor + boost_plus
+        return out_boost
+
+
+class KWinnersBoost(nn.Module):
+    def __init__(self, boost_method=PercentOfMaxBoosting()):
+        super().__init__()
+        self.boost_method = boost_method
+        self.func = _KWinnersBoostFunc()
+        self.func_apply = self.func.apply
+        self.register_parameter('boost_tensor', None)
+
+    def forward(self, tensor, sparsity=0.02, boost_percent=torch.tensor(1e-8)):
+        if boost_percent is None:
+            raise ValueError("boost_percent cannot be None.")
+        if self.boost_tensor is None:
+            self.boost_tensor = nn.Parameter(torch.zeros_like(tensor))
+
+        tensor, boost_tensor = self.func_apply(tensor, sparsity, self.boost_tensor, boost_percent, self.boost_method)
+        self.boost_tensor = nn.Parameter(boost_tensor)
+        return tensor
